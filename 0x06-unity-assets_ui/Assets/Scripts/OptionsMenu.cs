710f04f2013e368fc616748019eaad66b45fcf87
sranciato <sranciato1@gmail.com>
2020-06-22 23:22:07 -0400

    [ADD] pausecanvas

---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour {
+
+	int backScene;
+
+	public void Back()
+	{
+		SceneManager.LoadScene(backScene);
+	}
+
+	public void SetScene(int prevScene)
+	{
+		backScene = prevScene;
+	}
+}
