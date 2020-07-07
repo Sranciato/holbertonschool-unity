1afa2fde6b7ee6c70ee3c0b9f33903734f590844
sranciato <sranciato1@gmail.com>
2020-07-07 14:10:45 -0400

    [ADD] player model

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public GameObject timerCanvas;
+    public PlayerController playerController;
+    public GameObject mainCamera;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).length < animator.GetCurrentAnimatorStateInfo(0).normalizedTime)
+        {
+            timerCanvas.SetActive(true);
+            playerController.enabled = true;
+            mainCamera.SetActive(true);
+            gameObject.SetActive(false);
+            Debug.Log("Has Finsihed");
+        }
+    }
+}
